167daf0cb057cf8526e559b63604bdcde6218ba7
okircan97 <54515394+okircan97@users.noreply.github.com>
2024-04-24 15:04:36 +0300

    feat: Update the AddMovie page's styling
    Fixes #5

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import "./Styles/AddMovie.css";
 
 function AddMovie() {
   const [movieData, setMovieData] = useState({
@@ -35,41 +36,43 @@ function AddMovie() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Add Movie</h1>
-      <input
-        type="text"
-        name="movie_name"
-        value={movieData.movie_name}
-        onChange={handleChange}
-        placeholder="Movie Name"
-        required
-      />
-      <br />
-      <br />
-      <textarea
-        name="info"
-        value={movieData.info}
-        onChange={handleChange}
-        placeholder="Movie Info"
-        required
-      />
-      <br />
-      <br />
-      <input
-        type="number"
-        name="rating"
-        value={movieData.rating}
-        onChange={handleChange}
-        placeholder="Rating (1-10)"
-        min="1"
-        max="10"
-        required
-      />
-      <br />
-      <br />
-      <button type="submit">Add Movie</button>
-    </form>
+    <div className="background-add-movie">
+      <form onSubmit={handleSubmit} className="addMovieForm">
+        <h1>Add Movie</h1>
+        <input
+          type="text"
+          name="movie_name"
+          value={movieData.movie_name}
+          onChange={handleChange}
+          placeholder="Movie Name"
+          required
+        />
+        <br />
+        <br />
+        <textarea
+          name="info"
+          value={movieData.info}
+          onChange={handleChange}
+          placeholder="Movie Info"
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="number"
+          name="rating"
+          value={movieData.rating}
+          onChange={handleChange}
+          placeholder="Rating (1-10)"
+          min="1"
+          max="10"
+          required
+        />
+        <br />
+        <br />
+        <button type="submit">Add Movie</button>
+      </form>
+    </div>
   );
 }
 
